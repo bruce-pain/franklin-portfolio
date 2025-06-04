dd9b4f957c050f79070f4559e40fb71e49471f2d
Franklin Ikeh <ikehfranklin06@gmail.com>
2025-06-04 21:28:54 +0100

    feat: Add sitemap integration, update metadata for SEO, and include robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  adapter: vercel()
+    adapter: vercel(),
+    site: "https://franklin-ikeh-portfolio.vercel.app/",
+    integrations: [sitemap()],
 });
